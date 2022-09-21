740fa98c91e46c07fa7fa44ef8a971bf71595271
Weronika <WerRic96@protonmail.com>
2022-09-21 18:32:55 +0200

    Clear input when 'C' clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const container = document.querySelector(".container");
+const input = document.querySelector("input");
 
 function addEventToButtons() {
+    const clear = document.querySelector("#clear");
+    clear.addEventListener("click", clearInput);
+
     const typeButtons = document.querySelectorAll(".type");
     typeButtons.forEach(typeButton => {
         typeButton.addEventListener("click", typeInput)
@@ -8,8 +12,11 @@ function addEventToButtons() {
 }
 
 function typeInput(e) {
-    const input = document.querySelector("input");
     input.value += e.target.textContent;
+}
+
+function clearInput() {
+    input.value = "";
 }
 
 addEventToButtons();
