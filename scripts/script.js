7d6f3b0222667f435fb25b95384c56b89d834fd1
Weronika <WerRic96@protonmail.com>
2022-09-21 22:06:37 +0200

    Implement add function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
 const container = document.querySelector(".container");
 const input = document.querySelector("input");
+let numbers = [];
+let currentOperator;
 
 function addEventToButtons() {
     const clear = document.querySelector("#clear");
     clear.addEventListener("click", clearInput);
+
+    const operators = document.querySelectorAll(".operator");
+    operators.forEach(operator => {
+        operator.addEventListener("click", checkOperator);
+    });
 
     const typeButtons = document.querySelectorAll(".type");
     typeButtons.forEach(typeButton => {
@@ -11,8 +18,27 @@ function addEventToButtons() {
     });
 }
 
+function checkOperator(operator) {
+    numbers.push(Number(input.value));
+    clearInput();
+
+    if (numbers.length == 2) {
+        switch (currentOperator.target.value) {
+            case "+":
+                operate(add, numbers[0], numbers[1]);
+                break;
+        }
+    } else {
+       currentOperator = operator;
+    }
+    console.log(numbers);
+    console.log(currentOperator.target.value);
+
+}
+
 function typeInput(e) {
     input.value += e.target.textContent;
+
 }
 
 function clearInput() {
@@ -20,11 +46,15 @@ function clearInput() {
 }
 
 function operate(operator, numOne, numTwo) {
-    operator(numOne, numTwo);
+    const result = operator(numOne, numTwo);
+    numbers = [];
+    numbers.push(result);
+    console.log(numbers);
+
 }
 
 function add(numOne, numTwo) {
-    console.log("hello");
+    return numOne + numTwo;
 }
 
 addEventToButtons();
